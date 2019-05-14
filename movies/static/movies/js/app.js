d7c6dec1cf246769ba4560b26ecdc7f76d044819
unknown <onelife0985@gmail.com>
2019-05-14 20:04:52 +0900

    movie-list carousel

--- movies/static/movies/js/app.js
+++ movies/static/movies/js/app.js
@@ -1,9 +1,16 @@
 
-const movie_app = new Vue({
-    el: '#movie_app',
-    delimiters: ['${','}'],
-    data:{
-        hey:'Hello World',
-    }
+Vue.component('my-component', {
+    props:['title','text'],
+    template: '<div class="col"><div class="card"><img alt="" class="card-img-top img-fluid"><div class="card-body"><div class="card-title">{{title}}</div><div class="card-text">{{text}}</div></div></div></div>',
+});
 
-})
+const app = new Vue({
+    el: '#movie_app',
+    delimiters: ['${', '}'],
+    data: {
+        hey: 'Hello World',
+        img: '',
+        title: 'hey',
+        text: '',
+    },
+});
