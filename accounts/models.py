d08a6ee74f2ebc36aa602a4caf95a804247a1a1b
JAESEO LEE <goodstart57@gmail.com>
2019-05-16 05:36:05 +0900

    댓글 좋아요 기능 추가 (API)
    models.py : accounts의 User에 내가 좋아요한 댓글 필드 추가
    movies/urls.py : 주석으로 url 추가
    movies/vies.py : 좋아요/안좋아요 기능 views 함수 추가

--- accounts/models.py
+++ accounts/models.py
@@ -2,13 +2,14 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
 from people.models import People
-from movies.models import Movie, Genre
+from movies.models import Movie, Genre, Comment
 
 
 class User(AbstractUser):
     followings = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='followed')
     checking = models.ManyToManyField(Movie, related_name='checked')
     loving_people = models.ManyToManyField(People, related_name="loved")
+    like_comment = models.ManyToManyField(Comment, related_name="liked")
 
     def is_my_star(self, person):
         if not isinstance(person, People):
