ed6bdb97d8fce04c9fc0c096739b5968880fb644
unknown <onelife0985@gmail.com>
2019-05-17 13:47:49 +0900

    Django movie rendering -> javascript

--- final/static/js/script.js
+++ final/static/js/script.js
@@ -1,24 +1,27 @@
+const sess_storage_check = sessionStorage
 
 const nav_app = new Vue({
     delimiters:['${','}'],
     el:'#nav-app',
     data:{
-        base_url:'http://127.0.0.1:8000/api/v1/movies/',
+        base_url:location.origin+'/api/v1/movies/',
         user_search_input:'',
         movies:'',
         target_movies:[],
         origin_loc:location.origin,
         search_flag: true,
-        loading_full:false
+        loading_full:false,
+        storage: sess_storage_check,
     },
     methods:{
         searching:async function(){
             if(this.search_flag) {
                 const res = await axios.get(this.base_url);
-                console.log(res.data);
                 this.movies = res.data;
+                this.storage.setItem('movies', JSON.stringify(res.data));
                 this.search_flag = false;
             }
+
         },
         movie_url:function(movie_id){
             const url = `${this.origin_loc}/movies/${movie_id}/`;
@@ -42,6 +45,14 @@ const nav_app = new Vue({
                 }
             }
         }
+    },
+    beforeMount:function(){
+        try {
+            if (this.storage.getItem('movies')){
+                this.search_flag = false;
+                this.movies = JSON.parse(this.storage.getItem('movies'));
+            }
+        }catch{}
     }
 }
 
